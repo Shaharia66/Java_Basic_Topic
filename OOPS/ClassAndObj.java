24f66083d9745095e48cd9b48575f9c2c8d49156
Shaharia66 <gssaif.tm@gmail.com>
2026-01-06 14:08:36 +0600

    const

---
+++ OOPS/ClassAndObj.java
@@ -0,0 +1,31 @@
+package OOPS;
+
+class Pen{
+    String color;
+    String type;
+
+    public void write(){
+        System.out.println("write something");
+    }
+    public void printColor(){
+        System.out.println(this.color);
+    }
+}
+
+
+public class ClassAndObj {
+    public static void main(String[] args) {
+        Pen pen1 = new Pen();
+        pen1.color="red";
+        pen1.type="ballpen";
+        pen1.write();
+        pen1.printColor();
+
+        Pen pen2= new Pen();
+        pen2.color="blue";
+
+        pen2.printColor();
+
+    }
+    
+}
---
+++ OOPS/Constructors.java
@@ -0,0 +1,88 @@
+// // non parameterised constructor::
+// package OOPS;
+
+// class Student{
+//     String name ;
+//     int age ;
+
+//     public void printInfo(){
+//         System.out.println(this.name);
+//         System.out.println(this.age );
+//     }
+//     Student(){
+//         System.out.println("constructor called.");
+//     }
+// }
+
+// public class Constructors {
+//     public static void main(String[] args) {
+//         Student sd1=new Student();
+//         sd1.name="saif";
+//         sd1.age=26;
+//         sd1.printInfo();
+//     }
+// }
+
+
+
+// // parameterized Constructors::
+
+// package OOPS;
+
+// class Student{
+//     String name ;
+//     int age ;
+
+//     public void printInfo(){
+//         System.out.println(this.name);
+//         System.out.println(this.age );
+//     }
+//     Student(String name, int age){
+//         System.out.println("constructor called.");
+//         this.name=name;
+//         this.age=age;
+//     }
+// }
+
+// public class Constructors {
+//     public static void main(String[] args) {
+//         Student sd1=new Student("taif",26);
+//         sd1.printInfo();
+//     }
+// }
+
+
+
+// COPY  Constructors::
+
+package OOPS;
+
+class Student{
+    String name ;
+    int age ;
+
+    public void printInfo(){
+        System.out.println(this.name);
+        System.out.println(this.age );
+    }
+    Student(Student s2){
+        System.out.println("constructor called.");
+        this.name=s2.name;
+        this.age=s2.age;
+    }
+    Student(){
+
+    }
+}
+
+public class Constructors {
+    public static void main(String[] args) {
+        Student s1=new Student();
+        s1.name="rahim";
+        s1.age=25;
+
+        Student s2=new Student(s1);
+        s2.printInfo();
+    }
+}
+
