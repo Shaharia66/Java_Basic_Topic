077717523041128845e76c53c5b292c943d6a69f
Shaharia66 <gssaif.tm@gmail.com>
2026-01-29 14:35:21 +0600

    done oops

--- OOPS/Abstraction.java
+++ OOPS/Abstraction.java
@@ -1,34 +1,33 @@
-// package oops;
+//package oops
+abstract class Animal{
+    abstract void walk();
+    public void eat(){
+        System.out.println("animals eats");
+    }
+    Animal(){
+        System.out.println("animal's constructor .");
+    }
+}
+class Horse extends Animal{
+    public void walk(){
+        System.out.println("Horse walks on 4 legs ");
 
-// abstract class Animal{
-//     abstract void walk();
-//     public void eat(){
-//         System.out.println("animals eats");
-//     }
-//     Animal(){
-//         System.out.println("animal's constructor .");
-//     }
-// }
-// class Horse extends Animal{
-//     public void walk(){
-//         System.out.println("Horse walks on 4 legs ");
+    }
+    Horse(){
+        System.out.println("horse's constructors.");
+    }
+}
+class Chicken extends Animal{
+    public void walk(){
+        System.out.println("walks on 2 legs .");
+    }
+}
 
-//     }
-//     Horse(){
-//         System.out.println("horse's constructors.");
-//     }
-// }
-// class Chicken extends Animal{
-//     public void walk(){
-//         System.out.println("walks on 2 legs .");
-//     }
-// }
+public class Abstraction {
+    public static void main(String[] args) {
+        Horse h1=new Horse();
+        h1.eat();
+        h1.walk();
 
-// public class Abstraction {
-//     public static void main(String[] args) {
-//         Horse h1=new Horse();
-//         h1.eat();
-//         h1.walk();
-
-//     }
-// }
+    }
+}
